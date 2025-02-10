6930e1dcc612351dabf9570ca0e256861547e598
ouda-sadek <ouda.sadek@laplateforme.io>
2025-02-10 22:20:04 +0100

    init pygame, set up window, main file and gitignore

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from config import *
+#from game.states.menu import Menu
+
+# The main function that runs the game
+def main():
+
+    # Initialize pygame
+    pygame.init()
+
+    # Set up the window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Pokémon Game")
+
+    """menu = Menu(screen)
+    menu.afficher_menu()"""
+
+    # Main loop
+    running = True
+    while running:
+    # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+# Exit pygame
+pygame.quit()
+
+if __name__ == "__main__":
+    main()
