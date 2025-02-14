55913826b5de37e64bcd73b768522c2c63aede68
axel-achart <axel.achart@gmail.com>
2025-02-14 14:28:11 +0100

    rename musics, add load musics

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+
+
 # Screen dimensions
 SCREEN_WIDTH = 1150
 SCREEN_HEIGHT = 625
@@ -19,3 +25,9 @@ FPS = 60
 IMAGE_FOLDER = "./pokemon/assets/Images/"
 SOUND_FOLDER = "./pokemon/assets/sounds/"
 FONT_FOLDER = "./pokemon/assets/font/"
+
+# Grab musics
+MUSIC_MENU = pygame.mixer.Sound(SOUND_FOLDER + r"menu.mp3")
+MUSIC_FIGHT = pygame.mixer.Sound(SOUND_FOLDER + r"fight.mp3")
+MUSIC_ATTACK = pygame.mixer.Sound(SOUND_FOLDER + r"attack.mp3")
+MUSIC_END_GAME = pygame.mixer.Sound(SOUND_FOLDER + r"end_game.mp3")
